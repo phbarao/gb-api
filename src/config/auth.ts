ef44346804de6463166a1a13da5e5b2fdabeb13d
Pedro Barão <phbarao@gmail.com>
2021-03-21 23:57:42 -0300

    uploading images

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '2d2031ff75b0d1fc15f21032e7e64113',
+    expiresIn: '1d',
+  },
+};
---
+++ src/models/Appointments.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+// Toda vez que o model for salvo sera armazenado na tabela appointments
+// A classe é um parametro passado para entidade
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Appointment;
