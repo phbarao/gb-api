6a815da5ba22169c4816e74e6863e3a1b62b4c70
Pedro Barão <phbarao@gmail.com>
2021-05-17 19:21:31 -0300

    dependency injection

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,8 +7,8 @@ import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
-
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
