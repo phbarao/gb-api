ddc63a683353309c0d022bc96d07b2f6eff39838
Pedro Barão <phbarao@gmail.com>
2021-06-09 19:43:57 -0300

    setting aws ses

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'contato@barbertest.fun',
+      name: 'Equipe Barber Test',
+    },
+  },
+} as IMailConfig;
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -44,7 +44,7 @@ class SendForgotPasswordEmailService {
         name: user.name,
         email: user.email,
       },
-      subject: '[GoBarber] Recuperação de senha',
+      subject: '[Barber Test] Recuperação de senha',
       templateData: {
         file: forgotPasswordTemplate,
         variables: {
